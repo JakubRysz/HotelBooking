c28da78a1e9447beb37fbb8e650a1dd7b4b2e59a
Kuba <kuba.rysz10@gmail.com>
2024-02-25 20:10:46 +0100

    code tweaks

--- src/main/java/com/project/hotelBooking/domain/BookingInfo.java
+++ src/main/java/com/project/hotelBooking/domain/BookingInfo.java
@@ -1,14 +1,12 @@
 package com.project.hotelBooking.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class BookingInfo {
     private Booking booking;
     private Localization localization;
