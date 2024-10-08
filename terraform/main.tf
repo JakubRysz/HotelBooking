c7a54225218335cb4cc25a2d721fa3e260ea6983
Kuba <kuba.rysz10@gmail.com>
2024-10-08 22:13:17 +0200

    added backend_config.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.region
+}
+
 terraform {
     backend "s3" {
       bucket         = "tf-state-bucket-hotel-booking"
@@ -14,43 +18,8 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.region
-}
-
 data "aws_availability_zones" "available_zones" {
   state = "available"
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "tf-state-bucket-hotel-booking"
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket = aws_s3_bucket.terraform_state.bucket
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-locking"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
 }
 
 resource "aws_vpc" "vpc_1" {
