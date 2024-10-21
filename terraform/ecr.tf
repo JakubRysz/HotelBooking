738093f1829bb461b32532bd8dbc80b6643b219c
JakubRysz <73190553+JakubRysz@users.noreply.github.com>
2024-10-21 21:38:57 +0200

    Modify ECR lifecycle policy (#29)

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -6,39 +6,23 @@ resource "aws_ecr_repository" "ecr_1" {
   name = local.ecr_repository_name
 }
 
-resource "aws_ecr_lifecycle_policy" "ecr_lifecycle_policy_1" {
+resource "aws_ecr_lifecycle_policy" "ecr_lifecycle_policy" {
   repository = aws_ecr_repository.ecr_1.name
 
-  policy = <<EOF
-{
-  "rules": [
-    {
-      "rulePriority": 1,
-      "description": "Keep the latest image if there's only one image",
-      "selection": {
-        "tagStatus": "any",
-        "countType": "imageCountLessThan",
-        "countNumber": 2,
-        "countUnit": "images"
-      },
-      "action": {
-        "type": "expire"
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Limit to 3 most recent images"
+        selection = {
+          tagStatus    = "any"
+          countType    = "imageCountMoreThan"
+          countNumber  = 3
+        }
+        action = {
+          type = "expire"
+        }
       }
-    },
-    {
-      "rulePriority": 2,
-      "description": "Delete images older than one week",
-      "selection": {
-        "tagStatus": "any",
-        "countType": "sinceImagePushed",
-        "countNumber": 7,
-        "countUnit": "days"
-      },
-      "action": {
-        "type": "expire"
-      }
-    }
-  ]
-}
-EOF
+    ]
+  })
 }
