045cbb5be1e71767225fb960d60ecb4062ccbbb9
Kuba <kuba.rysz10@gmail.com>
2024-11-25 22:53:40 +0100

    updated GeneratedValue strategy in entity classes

--- src/main/java/com/project/hotelBooking/repository/model/Booking.java
+++ src/main/java/com/project/hotelBooking/repository/model/Booking.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 @Builder
 public class Booking {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="ID")
     private Long id;
 
--- src/main/java/com/project/hotelBooking/repository/model/Hotel.java
+++ src/main/java/com/project/hotelBooking/repository/model/Hotel.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Builder
 public class Hotel {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="ID")
      Long id;
     @Column(name="Name")
