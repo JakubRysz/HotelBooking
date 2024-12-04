a2745c50c81e1a34d5b84fa8c61a254381f1de22
Kuba <kuba.rysz10@gmail.com>
2024-12-04 22:21:02 +0100

    refactored Booking classes

--- src/main/java/com/project/hotelBooking/controller/mapper/BookingMapper.java
+++ src/main/java/com/project/hotelBooking/controller/mapper/BookingMapper.java
@@ -1,9 +1,6 @@
 package com.project.hotelBooking.controller.mapper;
 
-import com.project.hotelBooking.controller.model.BookingCreateAdminDto;
-import com.project.hotelBooking.controller.model.BookingCreateDto;
-import com.project.hotelBooking.controller.model.BookingDto;
-import com.project.hotelBooking.controller.model.BookingWithoutUserDto;
+import com.project.hotelBooking.controller.model.*;
 import com.project.hotelBooking.service.model.BookingServ;
 import org.mapstruct.Mapper;
 
@@ -13,5 +10,6 @@ public interface BookingMapper {
     BookingServ mapToBooking(BookingDto bookingDto);
     BookingServ mapToBooking(BookingCreateAdminDto bookingDto);
     BookingServ mapToBooking(BookingCreateDto bookingDto);
+    BookingServ mapToBooking(BookingEditDto bookingDto);
     BookingWithoutUserDto mapToBookingWithoutUserDto(BookingServ booking);
 }
---
+++ src/main/java/com/project/hotelBooking/controller/model/BookingEditDto.java
@@ -0,0 +1,14 @@
+package com.project.hotelBooking.controller.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@SuperBuilder
+public class BookingEditDto extends BookingBaseDto {
+    Long id;
+}
